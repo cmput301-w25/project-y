1e5d8b0339f63ee359e040d3f8594247836c9505
TomasOrdonezG <tomasordonezg@gmail.com>
2025-02-14 11:30:51 -0700

    Add follows model

---
+++ code/app/src/main/java/com/example/y/models/Follows.java
@@ -0,0 +1,33 @@
+package com.example.y.models;
+
+import com.google.firebase.Timestamp;
+
+import java.io.Serializable;
+
+/**
+ * Stores data of one user following another user.
+ */
+public class Follows implements Serializable {
+
+    private final String followerUsername;
+    private final String followedUsername;
+    private final Timestamp timestamp;
+
+    public Follows(String followerUsername, String followedUsername, Timestamp timestamp) {
+        this.followerUsername = followerUsername;
+        this.followedUsername = followedUsername;
+        this.timestamp = timestamp;
+    }
+
+    public String getFollowerUsername() {
+        return followerUsername;
+    }
+
+    public String getFollowedUsername() {
+        return followedUsername;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+}
