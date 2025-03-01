8e7928c68b2cc45397cd9d7c9fd72ba3c77b3452
tegen-ux <117418989+tegen-ux@users.noreply.github.com>
2025-02-28 20:24:33 -0700

    Added a profile page - work in progress

--- code/app/src/main/java/com/example/y/views/HomeActivity.java
+++ code/app/src/main/java/com/example/y/views/HomeActivity.java
@@ -1,17 +1,37 @@
 package com.example.y.views;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.y.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home); // Ensure this is your home.xml layout
+        setContentView(R.layout.home);
+    ImageButton profile = findViewById(R.id.btnUserProfile);
+
+    profile.setOnClickListener(v-> onProfileClick());
+
     }
+
+    private void onProfileClick(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+        //finish();
+    }
+
 }
 
 
