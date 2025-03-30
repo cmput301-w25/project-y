7a54de6570c7e37f2fa0726ab8107d69affa38c2
tegen-ux <117418989+tegen-ux@users.noreply.github.com>
2025-03-30 12:50:50 -0600

    Added some sleeps so that it doesn't instantly crash lol, also added it so that the text is random

--- code/app/src/androidTest/java/com/example/y/views/UpdateAndDeleteTest.java
+++ code/app/src/androidTest/java/com/example/y/views/UpdateAndDeleteTest.java
@@ -27,14 +27,12 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
-import androidx.test.rule.GrantPermissionRule;
 
 import com.example.y.R;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
-import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,25 +41,16 @@ import java.util.Random;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class AddUpdateDeleteActivityTest {
+public class UpdateAndDeleteTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
-    @Rule
-    public GrantPermissionRule mGrantPermissionRule =
-            GrantPermissionRule.grant(
-                    "android.permission.ACCESS_FINE_LOCATION",
-                    "android.permission.ACCESS_COARSE_LOCATION");
-
     @Test
-    public void addUpdateDeleteMoodEvent() throws InterruptedException {
+    public void updateAndDeleteTest() throws InterruptedException {
         Random rand = new Random();
         int x = rand.nextInt(100);
-
-
-        Thread.sleep(500);
         ViewInteraction appCompatEditText = onView(
                 allOf(withId(R.id.username),
                         childAtPosition(
@@ -70,7 +59,7 @@ public class AddUpdateDeleteActivityTest {
                                         0),
                                 1),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("testUser123"), closeSoftKeyboard());
+        appCompatEditText.perform(replaceText("testUser1"), closeSoftKeyboard());
 
         ViewInteraction appCompatEditText2 = onView(
                 allOf(withId(R.id.password),
@@ -82,6 +71,36 @@ public class AddUpdateDeleteActivityTest {
                         isDisplayed()));
         appCompatEditText2.perform(replaceText("testing"), closeSoftKeyboard());
 
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.username), withText("testUser1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText3.perform(click());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.username), withText("testUser1"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText4.perform(replaceText("testUser123"));
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.username), withText("testUser123"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatEditText5.perform(closeSoftKeyboard());
+
         ViewInteraction materialButton = onView(
                 allOf(withId(R.id.login_button), withText("Login"),
                         childAtPosition(
@@ -91,8 +110,7 @@ public class AddUpdateDeleteActivityTest {
                                 3),
                         isDisplayed()));
         materialButton.perform(click());
-        Thread.sleep(2000);
-
+        Thread.sleep(5000);
         ViewInteraction appCompatImageButton = onView(
                 allOf(withId(R.id.btnUserProfile), withContentDescription("User Profile"),
                         childAtPosition(
@@ -114,48 +132,14 @@ public class AddUpdateDeleteActivityTest {
                         isDisplayed()));
         floatingActionButton.perform(click());
 
-        ViewInteraction appCompatEditText3 = onView(
+        ViewInteraction appCompatEditText6 = onView(
                 allOf(withId(R.id.etReasonWhyText),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         0),
                                 1)));
-        appCompatEditText3.perform(scrollTo(), replaceText("test"+x), closeSoftKeyboard());
-
-        ViewInteraction appCompatSpinner = onView(
-                allOf(withId(R.id.spinnerMood),
-                        childAtPosition(
-                                allOf(withId(R.id.spinnerLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                2)),
-                                0)));
-        appCompatSpinner.perform(scrollTo(), click());
-
-        DataInteraction appCompatCheckedTextView = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(1);
-        appCompatCheckedTextView.perform(click());
-
-        ViewInteraction appCompatSpinner2 = onView(
-                allOf(withId(R.id.spinnerSocialSituation),
-                        childAtPosition(
-                                allOf(withId(R.id.spinnerLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                2)),
-                                1)));
-        appCompatSpinner2.perform(scrollTo(), click());
-
-        DataInteraction appCompatCheckedTextView2 = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(1);
-        appCompatCheckedTextView2.perform(click());
+        appCompatEditText6.perform(scrollTo(), replaceText(x+"testinglol"), closeSoftKeyboard());
 
         ViewInteraction materialTextView = onView(
                 allOf(withId(R.id.datePickerAddMood),
@@ -175,6 +159,23 @@ public class AddUpdateDeleteActivityTest {
                                 3)));
         materialButton2.perform(scrollTo(), click());
 
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.spinnerSocialSituation),
+                        childAtPosition(
+                                allOf(withId(R.id.spinnerLayout),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                2)),
+                                1)));
+        appCompatSpinner.perform(scrollTo(), click());
+
+        DataInteraction appCompatCheckedTextView = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(4);
+        appCompatCheckedTextView.perform(click());
+
         ViewInteraction materialButton3 = onView(
                 allOf(withId(R.id.btnSubmit), withText("Submit"),
                         childAtPosition(
@@ -187,22 +188,13 @@ public class AddUpdateDeleteActivityTest {
         materialButton3.perform(click());
         Thread.sleep(5000);
 
-        ViewInteraction textView = onView(
-                allOf(withId(R.id.text), withText("test"+x),
-                        withParent(withParent(withId(R.id.border))),
-                        isDisplayed()));
-        textView.check(matches(withText("test"+x)));
-        Thread.sleep(5000);
-
-
         DataInteraction linearLayout = onData(anything())
                 .inAdapterView(allOf(withId(R.id.listviewMoodEvents),
                         childAtPosition(
                                 withClassName(is("android.widget.LinearLayout")),
                                 1)))
-                .atPosition(1);
+                .atPosition(0);
         linearLayout.perform(click());
-
 
         ViewInteraction appCompatImageButton2 = onView(
                 allOf(withId(R.id.editMenuIcon), withContentDescription("editButton"),
@@ -214,43 +206,24 @@ public class AddUpdateDeleteActivityTest {
                         isDisplayed()));
         appCompatImageButton2.perform(click());
 
-        ViewInteraction appCompatEditText4 = onView(
-                allOf(withId(R.id.updateText), withText("test"+x),
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.updateText), withText(x+"testinglol"),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         0),
                                 1)));
-        appCompatEditText4.perform(scrollTo(), replaceText("test" + x*3));
+        appCompatEditText7.perform(scrollTo(), replaceText(x+"testing"));
 
-        ViewInteraction appCompatEditText5 = onView(
-                allOf(withId(R.id.updateText), withText("test"+x*3),
+        ViewInteraction appCompatEditText8 = onView(
+                allOf(withId(R.id.updateText), withText(x+"testing"),
                         childAtPosition(
                                 childAtPosition(
                                         withClassName(is("android.widget.LinearLayout")),
                                         0),
                                 1),
                         isDisplayed()));
-        appCompatEditText5.perform(closeSoftKeyboard());
-
-        ViewInteraction appCompatSpinner3 = onView(
-                allOf(withId(R.id.spinnerMoodUpdate),
-                        childAtPosition(
-                                allOf(withId(R.id.spinnerLayout),
-                                        childAtPosition(
-                                                withClassName(is("android.widget.LinearLayout")),
-                                                2)),
-                                0)));
-        appCompatSpinner3.perform(scrollTo(), click());
-
-        DataInteraction appCompatCheckedTextView3 = onData(anything())
-                .inAdapterView(childAtPosition(
-                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
-                        0))
-                .atPosition(7);
-        appCompatCheckedTextView3.perform(click());
-
-
+        appCompatEditText8.perform(closeSoftKeyboard());
 
         ViewInteraction materialButton4 = onView(
                 allOf(withId(R.id.UpdateMoodButton), withText("Update this mood"),
@@ -264,27 +237,19 @@ public class AddUpdateDeleteActivityTest {
         materialButton4.perform(click());
         Thread.sleep(5000);
 
-
-        ViewInteraction textView3 = onView(
-                allOf(withId(R.id.text), withText("test"+x*3),
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.text), withText(x+"testing"),
                         withParent(withParent(withId(R.id.border))),
                         isDisplayed()));
-        textView3.check(matches(withText("test"+x*3)));
-
-        ViewInteraction textView4 = onView(
-                allOf(withId(R.id.emoticon), withText("( ⊙_⊙)!"), withContentDescription("Emoticon"),
-                        withParent(withParent(IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class))),
-                        isDisplayed()));
-        textView4.check(matches(withText("( ⊙_⊙)!")));
+        textView.check(matches(withText(x+"testing")));
 
         DataInteraction linearLayout2 = onData(anything())
                 .inAdapterView(allOf(withId(R.id.listviewMoodEvents),
                         childAtPosition(
                                 withClassName(is("android.widget.LinearLayout")),
                                 1)))
-                .atPosition(1);
+                .atPosition(0);
         linearLayout2.perform(click());
-        Thread.sleep(5000);
 
         ViewInteraction appCompatImageButton3 = onView(
                 allOf(withId(R.id.editMenuIcon), withContentDescription("editButton"),
@@ -295,8 +260,6 @@ public class AddUpdateDeleteActivityTest {
                                 2),
                         isDisplayed()));
         appCompatImageButton3.perform(click());
-        Thread.sleep(5000);
-
 
         ViewInteraction materialButton5 = onView(
                 allOf(withId(R.id.deleteMoodButton), withText("Delete this mood"),
@@ -308,8 +271,32 @@ public class AddUpdateDeleteActivityTest {
                                 1),
                         isDisplayed()));
         materialButton5.perform(click());
+
+
+
         Thread.sleep(5000);
 
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withId(R.id.btnUserProfile), withContentDescription("User Profile"),
+                        childAtPosition(
+                                allOf(withId(R.id.header),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.RelativeLayout")),
+                                                0)),
+                                4),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        ViewInteraction materialButton6 = onView(
+                allOf(withId(R.id.logOutBtn), withText("Log Out"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        1),
+                                4),
+                        isDisplayed()));
+        materialButton6.perform(click());
     }
 
     private static Matcher<View> childAtPosition(
